81d6bd14562775fc71e74b44f0dd67155eaec0a7
庞博予 <pby17@mails.tsinghua.edu.cn>
2020-04-29 10:21:26 +0800

    ex1 base

--- src/ex1/func.py
+++ src/ex1/func.py
@@ -6,11 +6,13 @@ def cut(a, *args):
     type = args[0] if len(args) > 0 else 0
     if type == 0:
         return a
-    b = numpy.zeros(a.shape, dtype=float)
-    for i in range(a.shape[0]):
-        for j in range(a.shape[1]):
-            b[i][j] = a[i][j] if i <= 20 or j <= 20 else 0
-    return b
+    if type == 1:
+        b = numpy.zeros(a.shape, dtype=float)
+        for i in range(a.shape[0]):
+            for j in range(a.shape[1]):
+                b[i][j] = a[i][j] if i < (a.shape[0]>>1) and j < (a.shape[1]>>1) else 0
+        return b
+
 
 def dct(a, *args):
     type = args[0] if len(args) > 0 else 0
@@ -20,12 +22,20 @@ def dct(a, *args):
         b = numpy.zeros(a.shape, dtype = float)
         for i in range(a.shape[0] >> 3):
             for j in range(a.shape[1] >> 3):
-                bt = cv2.dct(a[(i<<3):(((i+1)<<3)), (j<<3):(((j+1)<<3))])
-                for i1 in range(8):
-                    for j1 in range(8):
-                        b[8*i+i1, 8*j+j1] = bt[i1, j1]
-                
+                b[i<<3:(i+1)<<3, j<<3:(j+1)<<3] = cv2.dct(a[(i<<3):(((i+1)<<3)), (j<<3):(((j+1)<<3))])
+
         return b
+    
+    if type == 2:
+        b = numpy.zeros(a.shape, dtype = float)
+        c = numpy.zeros(a.shape, dtype = float)
+        for i in range(a.shape[0]):
+            b[i, :(a.shape[1]>>1)] = cv2.dct(a[i, :]).T[0, :(a.shape[1]>>1)]
+
+        for i in range(a.shape[1] >> 1):
+            c[:(a.shape[0]>>1), i] = cv2.dct(b[:, i].T)[:(a.shape[0]>>1)].T
+        return c
+
     return a
 def idct(a, *args):
     type = args[0] if len(args) > 0 else 0
@@ -35,9 +45,15 @@ def idct(a, *args):
         b = numpy.zeros(a.shape, dtype = float)
         for i in range(a.shape[0] >> 3):
             for j in range(a.shape[1] >> 3):
-                bt = cv2.idct(a[(i<<3):(((i+1)<<3)), (j<<3):(((j+1)<<3))])
-                for i1 in range(8):
-                    for j1 in range(8):
-                        b[8*i+i1, 8*j+j1] = bt[i1, j1]
+                b[i<<3:(i+1)<<3, j<<3:(j+1)<<3] = cv2.idct(a[(i<<3):(((i+1)<<3)), (j<<3):(((j+1)<<3))])
+
         return b
+    if type == 2:
+        b = numpy.zeros(a.shape, dtype = float)
+        c = numpy.zeros(a.shape, dtype = float)
+        for i in range(a.shape[0]):
+            b[i, :]= cv2.idct(a[i, :])[:, 0]
+        for i in range(a.shape[1]):
+            c[:, i] = cv2.idct(b[:, i].T).T
+        return c
     return a
--- src/ex1/main.py
+++ src/ex1/main.py
@@ -1,6 +1,18 @@
 import numpy
 import cv2
 import func
+import sys
+import math
+import time
+
+choice = int(sys.argv[1]) if len(sys.argv) > 0 else 0
+configs = [(0, 0), (1, 0), (2, 0)]
+
+# 0:  2d-dct 100%           0   0           (auto)
+# 1:  2d-dct 8*8 100%       1   0           (solve)
+# 2:  1d-dct r&c 50%        2   0           (solve) 
+
+config = configs[choice]
 
 def show(d1, n1, d2, n2):
     cv2.imshow(n1, d1)
@@ -10,11 +22,22 @@ def show(d1, n1, d2, n2):
 d = cv2.imread('../../data/lena.bmp', 0)        #直接读入灰度图
 
 df_before = d.astype(numpy.float32)
-df_dct = func.dct(df_before, 0) 
 
-df_dct = func.cut(df_dct, 1)
-
-df_after = func.idct(df_dct, 0) 
-
+startTime = time.time_ns()
+df_dct = func.dct(df_before, config[0]) 
+#df_dct = func.cut(df_dct)
+#df_dct1 = func.dct(df_before, 0) 
+#df_dct1 = func.cut(df_dct1, 1);
+#show(df_dct1.astype(numpy.uint8), "2d-dct", (df_dct-df_dct1).astype(numpy.uint8), "1d-dct")
+df_after = func.idct(df_dct, config[0]) 
+useTime = time.time_ns()-startTime
 #print(df_after)
-show(d, "before", df_after.astype(numpy.uint8), "after")
+#show(d, "before", df_after.astype(numpy.uint8), "after")
+loss = df_before-df_after
+#print(loss)
+
+MSE = numpy.mean(loss ** 2)
+PSNR = 10*math.log(255*255/MSE, 10)
+print('MSE = ', MSE)
+print('PSNR = ', PSNR)
+print('Time = ', useTime / 1000000)
